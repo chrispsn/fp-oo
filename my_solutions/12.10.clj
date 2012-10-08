a54a82a3cdc03da1f5cc4ca391b202339c1e122b
Chris Pearson <chrispsn@gmail.com>
2012-10-08 23:36:21 +1100

    Cleaned ex 1 answer up a little

--- my_solutions/12.10.clj
+++ my_solutions/12.10.clj
@@ -4,30 +4,31 @@
 
 ; Exercise 1
 
-(def znode 
-    (fn [tree] (identity tree))
-)
+(defn znode [tree] (identity tree))
 
-(def zdown 
-    (fn [tree] (first tree))
-)
+(defn zdown [tree] (first tree))
 
 ; Tests
 (println 
-(-> 
-    '(a b c)
-    seq-zip
-    zdown
-    znode)
-; a
+    (-> 
+        '(a b c)
+        seq-zip
+        zdown
+        znode
+    ) ; a
 
-(-> 
-    '( (+ 1 2) 3 4) 
-    seq-zip 
-    zdown
-    znode)
-; (+ 1 2)
+    (-> 
+        '( (+ 1 2) 3 4) 
+        seq-zip 
+        zdown
+        znode
+    ) ; (+ 1 2)
 
-(-> '( (+ 1 2) 3 4) seq-zip zdown zdown znode)
-;; +
+    (-> 
+        '( (+ 1 2) 3 4) 
+        seq-zip 
+        zdown 
+        zdown 
+        znode
+    ) ; +
 )
