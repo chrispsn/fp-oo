cd313892d3bf7d7758e73bf264f6b1803d986f2a
Chris Pearson <chrispsn@gmail.com>
2012-10-08 23:48:49 +1100

    Exercise 2 in 12.10 done

--- my_solutions/12.10.clj
+++ my_solutions/12.10.clj
@@ -1,34 +1,52 @@
-; Starter pieces
+; Exercise 1
+
+; starter pieces
 (def seq-zip
     (fn [tree] tree))
 
-; Exercise 1
-
+; my answers
 (defn znode [tree] (identity tree))
 
 (defn zdown [tree] (first tree))
 
-; Tests
+; tests
 (println 
-    (-> 
-        '(a b c)
-        seq-zip
-        zdown
-        znode
-    ) ; a
-
-    (-> 
-        '( (+ 1 2) 3 4) 
-        seq-zip 
-        zdown
-        znode
-    ) ; (+ 1 2)
-
-    (-> 
-        '( (+ 1 2) 3 4) 
-        seq-zip 
-        zdown 
-        zdown 
-        znode
-    ) ; +
+    (-> '(a b c) seq-zip zdown znode)               ; a
+    (-> '( (+ 1 2) 3 4) seq-zip zdown znode)        ; (+ 1 2)
+    (-> '( (+ 1 2) 3 4) seq-zip zdown zdown znode)  ; +
 )
+
+
+; Exercise 2
+
+; starter pieces
+(def seq-zip
+    (fn [tree] {
+        :here tree
+        :parents '()
+    })
+)
+
+; my answers
+(defn znode [zipper] (:here zipper))
+
+(defn zdown [zipper] (assoc zipper
+        :here (first (:here zipper))
+        :parents (:here zipper)
+    )
+)
+
+; tests
+(println 
+    (-> '(a b c) seq-zip znode)         ; (a b c)
+    (-> '(a b c) seq-zip zdown znode)   ; a
+)
+
+
+; TEMPLATE
+
+; starter pieces
+
+; my answers
+
+; tests
